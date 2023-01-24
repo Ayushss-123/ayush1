f51690eeb0d38ccbe81029b09f3092fd07d8f924
Ayushss-123 <91745540+Ayushss-123@users.noreply.github.com>
2023-01-24 16:02:07 +0530

    Update layout.js

--- layout.js
+++ layout.js
@@ -2,11 +2,7 @@ import PropTypes from "prop-types";
   import React from "react";
   
   const Layout = ({ children }) => {
-    return <>{children}</>;
-  };
-  
-  Layout.propTypes = {
-    children: PropTypes.node.isRequired,
+ 
   };
   
   export default Layout;
